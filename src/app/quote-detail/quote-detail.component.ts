e5f29f60abc964c5e6484675d98b2911459c213d
SubiraMbula <subira.savali@student.moringaschool.com>
2022-01-11 23:32:31 +0300

    new files

--- src/app/quote-detail/quote-detail.component.ts
+++ src/app/quote-detail/quote-detail.component.ts
@@ -1,12 +1,19 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+// import {Quote} from '..quote'
+import { from } from 'rxjs';
 @Component({
-  selector: 'app-quote-detail',
-  templateUrl: './quote-detail.component.html',
-  styleUrls: ['./quote-detail.component.css']
+  selector: 'app-quote-details',
+  templateUrl: './quote-details.component.html',
+  styleUrls: ['./quote-details.component.css']
 })
-export class QuoteDetailComponent implements OnInit {
+export class QuoteDetailsComponent implements OnInit {
 
+@Input() quote:Quote;
+@Output() isComplete = new EventEmitter<boolean>();
+
+quoteDelete(complete:boolean){
+  this.isComplete.emit(complete)
+}
   constructor() { }
 
   ngOnInit(): void {

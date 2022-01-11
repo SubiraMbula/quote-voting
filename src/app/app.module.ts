07a2f5c50f9fcba3d9200bf5fcf3eca6a39bf1ef
SubiraMbula <subira.savali@student.moringaschool.com>
2022-01-11 23:29:56 +0300

    new files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QuoteDetailComponent } from './quote-detail/quote-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuoteDetailComponent
   ],
   imports: [
     BrowserModule,
